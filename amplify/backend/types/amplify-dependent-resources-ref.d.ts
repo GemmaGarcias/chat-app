509dcc072f27a793cb251fc9a3e02d989d81b736
Gemma <gemma.garcias.e@gmail.com>
2022-09-04 10:37:21 +0200

    Add chat source

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "chatapp": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "chatapp459c8097": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from '../aws-exports';
+Amplify.configure({...awsconfig, ssr: true });
+import '@aws-amplify/ui-react/styles.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
